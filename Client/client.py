57d8b1a0cf08e3af87387c6ca56d0d66054c76a3
Immanuellllllll <44088357+Immanuellllllll@users.noreply.github.com>
2021-09-16 11:10:08 +0200

    Client

---
+++ Client/client.py
@@ -0,0 +1,23 @@
+from bottle import run, get, post, view, response, request
+import requests
+import csv
+
+
+def send():
+    while(True):
+        print ("Enter number")
+        number = input()
+        xml = f"<?xml version='1.0' encoding='utf-8'?><data>{number}</data>"
+        headers = {'Content-Type': 'application/xml'} # set what your server accepts
+        response = requests.post('http://192.168.111.235:2222/receive-xml', data=xml, headers=headers)
+        readcsv(response.text.splitlines())
+
+def readcsv(csvdata):
+    reader = csv.reader(csvdata, delimiter=',')
+    for row in reader:
+        print(row)
+
+send()
+
+#run(host="127.0.0.1", port=1111, debug=False, reloader=True, server="paste")
+
